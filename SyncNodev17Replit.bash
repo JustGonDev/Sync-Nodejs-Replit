65a57d537a033e2e00c48d1b239d0dd3722daad7
Đặng Phong <thanhphongdang14@gmail.com>
2022-01-23 19:35:37 +0700

    Update SyncNodev17Replit.bash

--- SyncNodev17Replit.bash
+++ SyncNodev17Replit.bash
@@ -1,1 +1,1 @@
-npm init -y && npm i --save-dev node@16 && npm config set prefix=$(pwd)/node_modules/node && export PATH=$(pwd)/node_modules/node/bin:$PATH
+npm init -y && npm i --save-dev node@17 && npm config set prefix=$(pwd)/node_modules/node && export PATH=$(pwd)/node_modules/node/bin:$PATH
